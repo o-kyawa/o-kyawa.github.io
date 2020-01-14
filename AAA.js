c1784729c51c68863dc6bd29db74b49884aa22fb
o-kyawa <56067563+o-kyawa@users.noreply.github.com>
2020-01-14 21:56:22 +0900

    Create AAA.js

---
+++ AAA.js
@@ -0,0 +1,48 @@
+function Japan() {
+  console.log();
+  let answer = document.querySelector("#answer");
+  let answerHTML = "";
+  answer.innerHTML = '<iframe src="https://calendar.google.com/calendar/embed?src=86e19n3ak3vs2bgss6ne7rrjo4%40group.calendar.google.com&ctz=Asia%2FTokyo" style="border: 0" width="1000" height="500" frameborder="0" scrolling="no"></iframe>';
+}
+
+function England() {
+  console.log();
+  let answer = document.querySelector("#answer");
+  let answerHTML = "";
+  answer.innerHTML ='<iframe src="https://calendar.google.com/calendar/embed?src=hv2llbaacbo65apltrb5frp5n0%40group.calendar.google.com&ctz=Asia%2FTokyo" style="border: 0" width="1000" height="500" frameborder="0" scrolling="no"></iframe>';
+}
+
+function Itarly() {
+  console.log();
+  let answer = document.querySelector("#answer");
+  let answerHTML = "";
+  answer.innerHTML = '<iframe src="https://calendar.google.com/calendar/embed?src=uu0fuluqvvvapsupnfdequ7rdc%40group.calendar.google.com&ctz=Asia%2FTokyo" style="border: 0" width="1000" height="500" frameborder="0" scrolling="no"></iframe>';
+}
+
+function Germany() {
+  console.log();
+  let answer = document.querySelector("#answer");
+  let answerHTML = "";
+  answer.innerHTML = '<iframe src="https://calendar.google.com/calendar/embed?src=bde2l4juh2s68t1gti6q118b68%40group.calendar.google.com&ctz=Asia%2FTokyo" style="border: 0" width="1000" height="500" frameborder="0" scrolling="no"></iframe>';
+}
+
+function France() {
+  console.log();
+  let answer = document.querySelector("#answer");
+  let answerHTML = "";
+  answer.innerHTML = '<iframe src="https://calendar.google.com/calendar/embed?src=j124s5rf75oh3775s6ovtermqc%40group.calendar.google.com&ctz=Asia%2FTokyo" style="border: 0" width="1000" height="500" frameborder="0" scrolling="no"></iframe>';
+}
+
+function Spain() {
+  console.log();
+  let answer = document.querySelector("#answer");
+  let answerHTML = "";
+  answer.innerHTML = '<iframe src="https://calendar.google.com/calendar/embed?src=u25qpo2ajhju0g6pkubmj7k968%40group.calendar.google.com&ctz=Asia%2FTokyo" style="border: 0" width="1000" height="500" frameborder="0" scrolling="no"></iframe>';
+}
+
+function showAll() {
+  console.log();
+  let answer = document.querySelector("#answer");
+  let answerHTML = "";
+  answer.innerHTML = '<iframe src="https://calendar.google.com/calendar/embed?src=ubsdhnijad0ado895nk52c3qb4%40group.calendar.google.com&ctz=Asia%2FTokyo" style="border: 0" width="1000" height="500" frameborder="0" scrolling="no"></iframe>';
+}
